419da5a3487005455683a3af89c638798f341351
Juan Pablo <jpccdaw@@gmail.com>
2023-02-23 18:42:47 +0000

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 45);
+            $table->string('email', 45)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
+            $table->boolean('admin')->nullable();
+            $table->boolean('blocked')->nullable();
+            $table->boolean('validated')->nullable();
+            $table->decimal('coupon')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
