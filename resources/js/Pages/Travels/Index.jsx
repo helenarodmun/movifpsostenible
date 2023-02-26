86643721e756d586999b89aad06b404b785f507f
helenarodmun <helenarodmun@gmail.com>
2023-02-26 20:52:50 +0000

    Elementos react-boostrap

--- resources/js/Pages/Travels/Index.jsx
+++ resources/js/Pages/Travels/Index.jsx
@@ -19,49 +19,7 @@ export default function Index(props) {
                             </ul>
                         </div>
                     ))}
-                </Col>
-                <Col md={4}>
-                    <Card>
-                        <Card.Header>
-                            <h3>Publica un viaje</h3>
-                        </Card.Header>
-                        <Card.Body>
-                            <Form method="POST" action="/travels">
-                                <Form.Group>
-                                    <Form.Label>Origen:</Form.Label>
-                                    <Form.Control type="text" name="origin" placeholder="¿Cuál va a ser el punto de partida?" value={props.formValues.origin} onChange={props.handleInputChange} />
-                                    {props.errors.origin && <div className="alert alert-danger">{props.errors.origin}</div>}
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>Destino:</Form.Label>
-                                    <Form.Control type="text" name="destination" placeholder="¿Cuál va a ser el destino del viaje?" value={props.formValues.destination} onChange={props.handleInputChange} />
-                                    {props.errors.destination && <div className="alert alert-danger">{props.errors.destination}</div>}
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>Fecha:</Form.Label>
-                                    <Form.Control type="date" name="date" placeholder="Que día va a realizar el viaje?" value={props.formValues.date} onChange={props.handleInputChange} />
-                                    {props.errors.date && <div className="alert alert-danger">{props.errors.date}</div>}
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>Hora:</Form.Label>
-                                    <Form.Control type="text" name="hour" placeholder="¿A qué hora se iniciará el viaje?" value={props.formValues.hour} onChange={props.handleInputChange} />
-                                    {props.errors.hour && <div className="alert alert-danger">{props.errors.hour}</div>}
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>Asientos disponibles:</Form.Label>
-                                    <Form.Control as="select" name="seats" value={props.formValues.seats} onChange={props.handleInputChange}>
-                                        <option disabled>Escoja el número de asientos ...</option>
-                                        {props.seatsOptions.map((option) => (
-                                            <option key={option} value={option}>{option}</option>
-                                        ))}
-                                    </Form.Control>
-                                    {props.errors.seats && <div className="alert alert-danger">{props.errors.seats}</div>}
-                                </Form.Group>
-                                <Button variant="primary" onClick={props.handleSubmit}>Publicar viaje</Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                </Col>         
             </Row>
         </Container>
     );
