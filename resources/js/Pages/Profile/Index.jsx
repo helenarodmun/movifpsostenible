ad413e1d1d1807831518014a2168dcaa577d7e24
helenarodmun <helenarodmun@gmail.com>
2023-03-01 13:31:56 +0000

    Vista Profile/Index, con datos usuario

--- resources/js/Pages/Profile/Index.jsx
+++ resources/js/Pages/Profile/Index.jsx
@@ -1,26 +1,33 @@
 import { usePage } from "@inertiajs/react";
-import {  Container, Row, Col} from "react-bootstrap";
-import  Nav  from  '/resources/js/components/Nav';
-import  Footer  from  '/resources/js/components/Footer';
-import imgProfile from '/resources/assets/img/blank-profile.jpg'
+import { Container, Row, Col } from "react-bootstrap";
+import Nav from "/resources/js/components/Nav";
+import Footer from "/resources/js/components/Footer";
+import imgProfile from "/resources/assets/img/blank-profile.jpg";
 
 export default function Index() {
     const { auth } = usePage().props;
 
     return (
         <>
-        <Nav/>
-<Container className="m-5">   
-    <Row className="m-5">
-        <Col xl={12} className='mt-3 pt-3 shadow'>
-            <img className="float-left rounded-circle mr-2" src={imgProfile} width="120" height="120" alt='Imagen del perfil'></img>
-        <h1>{auth.user.name}</h1><br/>
-        <h3>{auth.user.email}</h3><br/>
-        </Col>
-    </Row>
-
-</Container>
-<Footer/>
-</>
+            <Nav />
+            <Container className="m-5">
+                <Row className="m-5">
+                    <Col xl={12} className="mt-3 pt-3 shadow">
+                        <img
+                            className="float-left rounded-circle mr-2"
+                            src={imgProfile}
+                            width="120"
+                            height="120"
+                            alt="Imagen del perfil"
+                        ></img>
+                        <h1>{auth.user.name}</h1>
+                        <br />
+                        <h3>{auth.user.email}</h3>
+                        <br />
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
+        </>
     );
-};
+}
--- resources/js/Pages/Travels/Search.jsx
+++ resources/js/Pages/Travels/Search.jsx
@@ -1,37 +1,49 @@
 import { usePage } from "@inertiajs/react";
 import React from "react";
 import { Button, Card, ListGroup } from "react-bootstrap";
-import  Nav  from  '/resources/js/components/Nav';
-import  Footer  from  '/resources/js/components/Footer';
+import Nav from "/resources/js/components/Nav";
+import Footer from "/resources/js/components/Footer";
 
 export default function Search() {
     const { travels } = usePage().props;
     return (
         <>
-        <Nav/>
+            <Nav />
             <h1 className="m-5">Viajes</h1>
             {travels.map((travel) => (
-                <Card   key={travel.id} className="m-5 ">
+                <Card key={travel.id} className="m-5 ">
                     <Card.Body>
-                        <Card.Header><h5>Detalles del viaje</h5></Card.Header>
-                        <Card.Text >
-                        <ListGroup variant="flush">
-                                <ListGroup.Item>Origen: {travel.origin}</ListGroup.Item>
-                                <ListGroup.Item>Destino: {travel.destination}</ListGroup.Item>
-                                <ListGroup.Item>Fecha: {travel.date}</ListGroup.Item>
-                                <ListGroup.Item>Hora: {travel.hour}</ListGroup.Item>
-                                <ListGroup.Item>Asientos disponibles: {travel.seats}</ListGroup.Item>
+                        <Card.Header>
+                            <h5>Detalles del viaje</h5>
+                        </Card.Header>
+                        <Card.Text>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    Origen: {travel.origin}
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Destino: {travel.destination}
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Fecha: {travel.date}
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Hora: {travel.hour}
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Asientos disponibles: {travel.seats}
+                                </ListGroup.Item>
                                 <small>
                                     Publicado por: {travel.driver.name}{" "}
                                     {travel.updated_at}
                                 </small>
-                                </ListGroup>  
+                            </ListGroup>
                         </Card.Text>
                         <Button variant="success">Reserva tu viaje!</Button>
                     </Card.Body>
                 </Card>
             ))}
-            <Footer/>
+            <Footer />
         </>
     );
 }
