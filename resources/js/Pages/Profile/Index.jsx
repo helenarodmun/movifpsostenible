b244df0946f9249f917f293624d29ea5a1a07656
helenarodmun <helenarodmun@gmail.com>
2023-03-01 22:41:02 +0000

    Trabajando con la vista de Profile, datos de la bd y botón de edición

--- resources/js/Pages/Profile/Index.jsx
+++ resources/js/Pages/Profile/Index.jsx
@@ -1,12 +1,16 @@
 import { usePage } from "@inertiajs/react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 import Nav from "/resources/js/components/Nav";
 import Footer from "/resources/js/components/Footer";
 import imgProfile from "/resources/assets/img/blank-profile.jpg";
 
 export default function Index() {
     const { auth } = usePage().props;
-
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+          Editar perfil
+        </Tooltip>
+      );
     return (
         <>
             <Nav />
@@ -22,13 +26,24 @@ export default function Index() {
                         ></img>
                         <h1>{auth.user.name}</h1>
                         <br />
-                        <h3>{auth.user.centro}</h3>
+                        <h2><small>Centro educativo:</small>{auth.user.centro}</h2>
                         <br />
-                        <h4>{auth.user.email}</h4>
+                        <h4><small>Correo:</small>{auth.user.email}</h4>
                         <br />
-                        <h4>{auth.user.tags}</h4>
+                        <h4><small>Tags:</small>{auth.user.tags}</h4>
                         <br />
-                        <h4>{auth.user.description}</h4>
+                        <h4><small>Descripción:</small>{auth.user.description}</h4>
+                        <>
+                        <OverlayTrigger
+        placement="bottom"
+        delay={{ show: 250, hide: 400 }}
+        overlay={renderTooltip}
+      >
+        <Button className="btn btn-secondary float-right m-3">
+          <i className="bi bi-pencil-square"></i>
+        </Button>
+      </OverlayTrigger>
+                        </>
                     </Col>
                 </Row>
             </Container>
