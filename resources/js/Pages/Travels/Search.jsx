763b047e9a4d87a7d192cda93d613b1844a3a8ff
helenarodmun <helenarodmun@gmail.com>
2023-03-01 11:23:47 +0000

    Visualización en Search de los datos de la consulta, añadido botón reserva y elementos bootstrap

--- resources/js/Pages/Travels/Search.jsx
+++ resources/js/Pages/Travels/Search.jsx
@@ -1,31 +1,33 @@
-import { usePage } from '@inertiajs/react';
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import { usePage } from "@inertiajs/react";
+import React from "react";
+import { Button, Card, ListGroup } from "react-bootstrap";
 
 export default function Search() {
-  const {travels} = usePage().props
-  return (
-    
-    <>
-      <h1>Viajes</h1>
-      <Card>
-        <Card.Body>
-          <Card.Title>Detalles del viaje</Card.Title>
-          <Card.Text>
-          {console.log(travels)}
-                        {travels.map((travel) => (
-                            <div key={travel.id}>
-            <li>Origen: {travel.origin}</li>
-            <li>Destino: {travel.destination}</li>
-            <li>Fecha: {travel.date}</li>
-            <li>Hora: {travel.hour}</li>
-            <li>Asientos disponibles: {travels.seats}</li>
-            <small>Publicado por: {travel.driver.name} {travel.updated_at}</small>
-            </div>
-                        ))}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </>
-  );
+    const { travels } = usePage().props;
+    return (
+        <>
+            <h1>Viajes</h1>
+            {travels.map((travel) => (
+                <Card   key={travel.id} className="m-5 ">
+                    <Card.Body>
+                        <Card.Header><h5>Detalles del viaje</h5></Card.Header>
+                        <Card.Text >
+                        <ListGroup variant="flush">
+                                <ListGroup.Item>Origen: {travel.origin}</ListGroup.Item>
+                                <ListGroup.Item>Destino: {travel.destination}</ListGroup.Item>
+                                <ListGroup.Item>Fecha: {travel.date}</ListGroup.Item>
+                                <ListGroup.Item>Hora: {travel.hour}</ListGroup.Item>
+                                <ListGroup.Item>Asientos disponibles: {travel.seats}</ListGroup.Item>
+                                <small>
+                                    Publicado por: {travel.driver.name}{" "}
+                                    {travel.updated_at}
+                                </small>
+                                </ListGroup>  
+                        </Card.Text>
+                        <Button variant="success">Reserva tu viaje!</Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+    );
 }
