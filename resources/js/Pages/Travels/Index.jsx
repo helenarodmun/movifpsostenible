e791cd50ad23818730554ce4a5b8743580583a05
Juan Pablo <jpccdaw@@gmail.com>
2023-03-01 13:12:25 +0000

    vista ndex estilizada

--- resources/js/Pages/Travels/Index.jsx
+++ resources/js/Pages/Travels/Index.jsx
@@ -1,36 +1,53 @@
 import { usePage } from "@inertiajs/react";
 // import React from "react";
 import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
-import Nav from '../../components/Nav'
+import Nav from "../../components/Nav";
 export default function Index() {
-    const {travels} = usePage().props
+    const { travels } = usePage().props;
+    function myDate(fechaHora) {
+        const date =  new Date(fechaHora);
+
+    }
     return (
         <>
             <Nav></Nav>
             <Container>
                 <Row>
-                    <Col md={8}>
-                        <h1>Viajes</h1>
-                        {console.log(travels)}
-                        {travels.map((travel) => (
+                    <h1>Viajes</h1>
+                    {travels.map((travel) => (
+                        <Col md={6} className="-ml-3">
                             <div key={travel.id}>
-                                {console.log(travel.driver.name)}
-                                <ul>
-                                    <li>Origen: {travel.origin}</li>
-                                    <li>Destino: {travel.destination}</li>
-                                    <li>Fecha: {travel.date}</li>
-                                    <li>Hora: {travel.hour}</li>
-                                    <li>
-                                        Asientos disponibles: {travel.seats}
-                                    </li>
-                                    <small>
-                                        Publicado por: {travel.driver.name}{" * "}
-                                        {travel.updated_at}
-                                    </small>
-                                </ul>
+                                <Card>
+                                    <Card.Title className="m-4">
+                                        Origen: {travel.origin}
+                                        <br />
+                                        Destino: {travel.destination}
+                                    </Card.Title>
+                                    <Card.Body>
+                                        <span>Fecha: {travel.date}</span>
+                                        {" / "}
+                                        <span>Hora: {travel.hour}</span>
+                                        {" / "}
+                                        <span>
+                                            {" "}
+                                            Asientos disponibles: {travel.seats}
+                                        </span>
+                                    </Card.Body>
+                                    <Card.Footer className="text-muted">
+                                        Publicado por:{" "}
+                                        <a
+                                            href="/profile"
+                                            className="text-decoration-none text-base   "
+                                        >
+                                            {travel.driver.name}
+                                        </a>
+                                        {/* {" - "} */}
+                                        {/* {travel.updated_at} */}
+                                    </Card.Footer>
+                                </Card>
                             </div>
-                        ))}
-                    </Col>
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </>
